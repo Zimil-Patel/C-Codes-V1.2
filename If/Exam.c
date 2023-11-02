a626fa2643c3923241811cf48c2181f353cabb1f
Zimil-Patel <patelzimil2310@gmail.com>
2023-11-02 21:16:05 +0530

    02-11-2023

---
+++ If/Exam.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main(){
+
+    int x = 10;
+
+    if(x == 5)
+        if(x < 10)
+            printf("yes in");
+        else
+            printf("yes else");
+
+    else
+        printf("no else");
+    
+
+}
