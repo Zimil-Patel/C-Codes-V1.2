9651fbbf6130a9ff7456b5718720295090b20cb6
Zimil-Patel <patelzimil2310@gmail.com>
2023-11-05 21:54:48 +0530

    05-11-2023

---
+++ FinalExamC/Sample.c
@@ -0,0 +1,187 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct LoginSystem
+{
+    char gmail[50];
+    char password[32];
+};
+int checkGmail(char gmail[50])
+{
+    int length = strlen(gmail);
+    char lastMail[11] = "moc.liamg@";
+    if (length < 50)
+    {
+        int a = 0;
+        int checkLast = 0;
+        for (int i = length - 1; i > length - 11; i--)
+        {
+            if (gmail[i] != lastMail[a])
+            {
+                checkLast = 1;
+                break;
+            }
+            a++;
+        }
+        if (checkLast == 0)
+        {
+            int checkStart = 0;
+            for (int i = 0; i < length - 11; i++)
+            {
+                if ((gmail[i] >= 33 && gmail[i] <= 47) || (gmail[i] >= 58 && gmail[i] <= 64) || (gmail[i] >= 91 && gmail[i] <= 96) || gmail[i] == 32)
+                {
+                    checkStart = 1;
+                    break;
+                }
+            }
+            if (checkStart == 0)
+            {
+                printf("Email Varified !\n");
+                return 1;
+            }
+            else
+            {
+                printf("special character or space not allow in mail address !\n");
+            }
+        }
+        else
+        {
+            printf("invalid @gmail.com format\n");
+        }
+    }
+    else
+    {
+        printf("Invalid Length ! - Your mail length is greter than 50 letters !\n");
+    }
+}
+
+int checkPassword(char password[32])
+{
+    int length = strlen(password);
+    if (length > 8)
+    {
+        int checkSpecialCharacter = 0;
+        for (int i = 0; i < length; i++)
+        {
+            if ((password[i] >= 33 && password[i] <= 47) || (password[i] >= 58 && password[i] <= 64) || (password[i] >= 91 && password[i] <= 96))
+            {
+                checkSpecialCharacter = 1;
+                break;
+            }
+        }
+        if (checkSpecialCharacter == 1)
+        {
+            int checkCapitalLetter = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (password[i] >= 65 && password[i] <= 90)
+                {
+                    checkCapitalLetter = 1;
+                    break;
+                }
+            }
+            if (checkCapitalLetter == 1)
+            {
+                int checkSmallLetter = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    if (password[i] >= 97 && password[i] <= 122)
+                    {
+                        checkSmallLetter = 1;
+                        break;
+                    }
+                }
+                if (checkSmallLetter == 1)
+                {
+                    int checkNumber = 0;
+                    for (int i = 0; i < length; i++)
+                    {
+                        if (password[i] >= 49 && password[i] <= 57)
+                        {
+                            checkNumber = 1;
+                            break;
+                        }
+                    }
+                    if (checkNumber == 1)
+                    {
+                        printf("password verified !\n");
+                        return 1;
+                    }
+                    else
+                    {
+                        printf("password should be contain at least one number !\n");
+                    }
+                }
+                else
+                {
+                    printf("password should be contain at least one small letter !\n");
+                }
+            }
+            else
+            {
+                printf("password should be contain at least one capital letter !\n");
+            }
+        }
+        else
+        {
+            printf("password should be contain at least one special character !\n");
+        }
+    }
+    else
+    {
+        printf("password length should be greater tha  8 character !\n");
+    }
+}
+
+int main()
+{
+    FILE *filePointer;
+
+    filePointer = fopen("login.txt", "w");
+
+    struct LoginSystem l1[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Register your mail : ");
+        gets(l1[i].gmail);
+        checkGmail(l1[i].gmail);
+        printf("Register your password : ");
+        gets(l1[i].password);
+        checkPassword(l1[i].password);
+        printf("\n\n");
+    }
+
+    // login system
+
+    char gmail[50];
+    char password[32];
+    int status = 0;
+
+    printf("\n-----------------Login Your Account-----------------\n");
+    printf("Enter your mail : ");
+    gets(gmail);
+    printf("Enter your password : ");
+    gets(password);
+    int finalCheck = 0;
+    for (int i = 0; i < 3; i++)
+    {
+
+        int x = strcmp(gmail, l1[i].gmail);
+        int y = strcmp(password, l1[i].password);
+
+        if (x == 0 && y == 0)
+        {
+            finalCheck = 1;
+            printf("Login Successful !");
+            fprintf(filePointer, "Email : %s\nPassword : %s", l1[i].gmail, l1[i].password);
+        }
+    }
+
+    if (finalCheck == 0)
+    {
+        printf("Login Failed !");
+    }
+
+    return 0;
+}
--- FinalExamC/UserLoginSystem.c
+++ FinalExamC/UserLoginSystem.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
 int i, j;
 
@@ -21,9 +22,12 @@ int checkDomain(char gmail[]){
         
         for (i = strlen(gmail) - 10; i < strlen(gmail); i++){
 
-            if(gmail[i] != domain[j])
-                return 0;
+            if(gmail[i] != domain[j]){
 
+                puts("Invalid!!! domain name must be '@gmail.com'!");
+                return 0;
+            }
+            
             j++;
 
         }
@@ -32,7 +36,7 @@ int checkDomain(char gmail[]){
 
     } else {
 
-        puts("Invalid! too small email Length");
+        puts("\nInvalid!!! too small email Length!");
         return 0;
 
     }
@@ -43,12 +47,169 @@ int checkDomain(char gmail[]){
 //Check full gmailAddress
 int checkFullGmail(char gmail[]){
 
-    for (i = strlen(gmail); i < strlen(gmail) - 10; i++){
+    int checkGmail = 0;
+    //Check For Alpahbets
+    for (i = 0; i < strlen(gmail) - 10; i++)
+        if((gmail[i] >= 'A' && gmail[i] <= 'Z') 
+        || (gmail[i] >= 'a' && gmail[i] <= 'z')){
+    
+            checkGmail = 1;
+            break;
+    
+        }
+            
 
-        if(gmail[i] != domain[j])
-             return 0;
 
-        j++;
+    if(checkGmail){
+
+        checkGmail = 0;
+        //check For Digits
+        for (i = 0; i < strlen(gmail) - 10; i++)
+            if(gmail[i] >= '0' && gmail[i] <= '9'){
+    
+                checkGmail = 1;
+                break;
+    
+            }
+
+        if(checkGmail){
+
+            checkGmail = 0;
+            //check For Space
+            for (i = 0; i < strlen(gmail) - 10; i++)
+                if((gmail[i] >= 33 && gmail[i] <= 47) 
+                || (gmail[i] >= 58 && gmail[i] <= 64) 
+                || (gmail[i] >= 91 && gmail[i] <= 96) 
+                || gmail[i] == 32){
+    
+                    checkGmail = 1;
+                    break;
+        
+                }
+
+            if(checkGmail){
+
+                puts("\nInvalid!!! Gmail must not contain 'space' or 'Special Character'!");
+                return 0;
+
+            } else {
+
+                return 1;
+
+            }
+
+        } else {
+
+            puts("\nInvalid!!! Gmail must include 'Digits'!");
+            return 0;
+
+        }
+
+    } else {
+
+        puts("\nInvalid!!! Gmail must include 'Alphabets'!");
+        return 0;
+
+    }
+
+}
+
+//check Full Password
+int checkFullPassword(char pass[]){
+
+    int checkPass = 0;
+    int passLength = strlen(pass);
+
+    //Check For Length
+    if (passLength < 8 || passLength > 32){
+
+        puts("\nInvalid!!! Password Length!");
+        return 0;
+
+    } else {
+
+        //Check For Capital Alphabet
+        for (i = 0; i < passLength; i++)
+            if(pass[i] >= 'A' && pass[i] <= 'Z'){
+
+                checkPass = 1;
+                break;
+
+            }
+                
+
+        if (checkPass){
+
+            checkPass = 0;
+            //Check For Small Alphabet
+            for (i = 0; i < passLength; i++)
+                if(pass[i] >= 'a' && pass[i] <= 'z'){
+
+                    checkPass = 1;
+                    break;
+
+                }
+                    
+
+            if (checkPass){
+
+                checkPass = 0;
+                //Check For Digits
+                for (i = 0; i < passLength; i++)
+                    if(pass[i] >= '0' && pass[i] <= '9'){
+
+                        checkPass = 1;
+                        break;
+
+                    }
+
+                if(checkPass){
+
+                    checkPass = 0;
+                    // Check For Special Character
+                    for (i = 0; i < passLength; i++)
+                        if((pass[i] >= 33 && pass[i] <= 47) 
+                        || (pass[i] >= 58 && pass[i] <= 64) 
+                        || (pass[i] >= 91 && pass[i] <= 96)){
+
+                            checkPass = 1;
+                            break;
+
+                        }
+
+                    if (checkPass){
+
+                        puts("Registration Successful!");
+                        return 1;
+
+                    } else {
+
+                        puts("\nInvalid!!! Password must contain at least one special character!");
+                        return 0;
+
+                    }
+
+                } else {
+
+                    puts("\nInvalid!!! Password must contain 'Digits'!");
+                    return 0;
+
+                }
+                        
+
+            } else {
+
+                puts("\nInvalid!!! Password must contain 'Small alphabet'!");
+                return 0;
+
+            }
+
+        } else {
+
+            puts("\nInvalid!!! Password must contain 'Capital alphabet'!");
+            return 0;
+
+        }
 
     }
 
@@ -58,20 +219,82 @@ int checkFullGmail(char gmail[]){
 //mainMethod
 void main(){
 
-    struct UserInfo user1;
+    struct UserInfo user;
 
-    int gmailCheck;
+    int checkGmail, checkPass;
 
-    printf("\n----Create new user----\n");
-    printf("\nEnter email address : ");
-    scanf(" %[^\n]", &user1.userGmail);
+    char loginGmail[32], loginPass[32];
 
-    gmailCheck = checkDomain(user1.userGmail);
+    FILE *filePointer;
 
-    if(gmailCheck)
-        gmailCheck = checkFullGmail(user1.userGmail);
+    filePointer = fopen("LoginRecords.txt", "w");
 
+        //Registration -  Taking gmail from user (with validation)
+        printf("\n----Create new user----\n");
 
+        getGmail:
+        printf("\n---------------\n");
+        printf("Create email : ");
+        scanf(" %[^\n]", &user.userGmail);
 
+        checkGmail = checkDomain(user.userGmail);
+
+        if(checkGmail)
+            checkGmail = checkFullGmail(user.userGmail);
+        else
+            goto getGmail;
+
+        //Registration - Taking Password from user (with validation)
+        if(checkGmail){
+
+            getPass:
+            printf("Create Password : ");
+            scanf(" %[^\n]", &user.userPass);
+
+            checkPass = checkFullPassword(user.userPass);
+
+            if(checkPass == 0){
+
+                printf("\n---------------\n");
+                goto getPass;
+
+            }
+
+        }
+                
+    printf("\n-------Login--------\n");
+
+    for (i = 0; i < 3; i++){
+
+        printf(":Attempt %d:\n", i + 1 );
+
+        printf("Enter email : ");
+        scanf(" %[^\n]", &loginGmail);
+
+        printf("Enter password : ");
+        scanf(" %[^\n]", &loginPass);
+
+        fprintf(filePointer, "User Email : %s",loginGmail);
+        fprintf(filePointer, "\nUser Password : %s",loginPass);
+
+        if (strcmp(loginGmail, user.userGmail) == 0 
+        && strcmp(loginPass, user.userPass) == 0)
+        {
+            fprintf(filePointer, "\n-----------------");
+            puts("Login Successfull");
+            fprintf(filePointer, "\nLogin Successfull\n");
+            fprintf(filePointer, "-----------------\n");
+            break;
+
+        } else {   
+            fprintf(filePointer, "\n-----------------------------");
+            puts("Invalid username or password!");
+            fprintf(filePointer, "\nInvalid username or password!\n");
+            fprintf(filePointer, "-----------------------------\n");
+            continue;
+
+        }
+
+    }
 
 }
